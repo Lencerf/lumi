c048bebb1993f25ac66ef530c43d4b5f2a6be3c6
Lencerf <changyuan.lv@gmail.com>
2021-04-01 00:34:29 -0400

    Add a binary target for producing reports

---
+++ src/bin/lumi.rs
@@ -0,0 +1,51 @@
+use clap::{clap_app, ArgMatches};
+use lumi::ledger::Ledger;
+use rust_decimal::prelude::Zero;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const AUTHOR: &str = env!("CARGO_PKG_AUTHORS");
+
+fn balances(matches: &ArgMatches) {
+    let path = matches.value_of("INPUT").unwrap();
+    let (ledger, errors) = Ledger::from_file(path);
+
+    for error in errors {
+        println!("{}\n", error);
+    }
+
+    let mut result = vec![];
+    for (account, account_map) in ledger.balance_sheet() {
+        if ledger.accounts[account].close.is_some() {
+            continue;
+        }
+        for (currency, currency_map) in account_map {
+            for (cost, number) in currency_map {
+                if number.is_zero() {
+                    continue;
+                }
+                if let Some(cost) = cost {
+                    result.push(format!("{} {} {} {}", account, number, currency, cost));
+                } else {
+                    result.push(format!("{} {} {}", account, number, currency));
+                }
+            }
+        }
+    }
+    result.sort();
+    for entry in result {
+        println!("{}", entry);
+    }
+}
+fn main() {
+    let matches = clap_app!(lumi =>
+        (version: VERSION)
+        (author: AUTHOR)
+        (@subcommand balances =>
+            (@arg INPUT: +required "Input file")
+        )
+    )
+    .get_matches();
+    if let Some(matches) = matches.subcommand_matches("balances") {
+        balances(&matches);
+    }
+}
