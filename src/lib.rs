867386581812164caf6afc4090b3bb44e5408577
Lencerf <changyuan.lv@gmail.com>
2021-04-04 15:32:59 -0400

    Add fields and doc url for publishing

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 //!
 //! lumi is a double-entry accounting tool, and a library for parsing text-based
 //! ledger files.
+#![doc(html_root_url = "https://docs.rs/lumi/0.1.0")]
 
 mod ledger;
 mod options;
