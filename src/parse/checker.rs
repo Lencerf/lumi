2828693ab82ce48e41f2599aa467cc047b68340e
Lencerf <changyuan.lv@gmail.com>
2022-09-18 18:02:52 -0700

    feat(checker): sort postings by account names

--- src/parse/checker.rs
+++ src/parse/checker.rs
@@ -648,6 +648,7 @@ fn check_complete_txn(
             return Err(e);
         }
     }
+    valid_postings.sort_by(|p1, p2| {p1.account.cmp(&p2.account) });
     let valid_txn = Transaction {
         date,
         flag,
